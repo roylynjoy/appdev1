3d348ad9d664392f847ec6e44070f53170e18906
Roylyn <roylynjoyestrera@gmail.com>
2024-10-09 15:30:10 +0800

    I’m done completing the section on writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -4,17 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
+function AboutPage() {
+  return(
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
   );
 }
+
 function App() {
 
   return (
     <>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <AboutPage />
     </>
   )
 }
