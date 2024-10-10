112bb15f28e4d64444da093ac0ee26ec3bc531dc
Roylyn <roylynjoyestrera@gmail.com>
2024-10-10 20:17:15 +0800

    I’m done completing the section on updating the screen

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AdminPanel from './AdminPanel'
-import LoginForm from './LoginForm'
+import { useState } from 'react';
 
 function MyButton() {
+  const [count, setCount] = useState(0);
+
   function handleClick() {
-    alert('You clicked me!');
+    setCount(count + 1);
   }
 
   return (
     <button onClick={handleClick}>
-      Click me
+      Clicked {count} times
     </button>
   );
 }
 
-export default MyButton
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+
+
 
