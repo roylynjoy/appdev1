a0c65ac6383f370acbc335dd5c802448716cbefc
Roylyn <roylynjoyestrera@gmail.com>
2024-10-10 19:25:12 +0800

    I'm done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -4,23 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function AboutPage() {
-  return(
-    <>
-      <img className='avatar' src='https://i.imgur.com/yXOvdOSs.jpg'/>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
 }
 
-function App() {
+function Profile() {
 
   return (
     <>
-      <AboutPage />
+      <h1>{user.name}</h1>
+      <img className = "avatar"
+      src = {user.imageUrl}
+      alt = {'Photo of ' + user.name}
+      style = {{
+        width: user.imageSize,
+        height: user.imageSize
+      }}
+      />
     </>
   )
 }
 
-export default App
+export default Profile
