b19c27fe59165a0ec429bd7c9e9640cce05b0978
Roylyn <roylynjoyestrera@gmail.com>
2024-11-16 13:56:35 +0800

    Done Toggle Button

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Counter from './counter.jsx'
+import ToggleButton from './toggleButton.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
+    <ToggleButton />
   </StrictMode>,
 )
